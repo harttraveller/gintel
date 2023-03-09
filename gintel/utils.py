6c9c0593cc7487d808477b719e14c56c21f5b132
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-08 23:14:18 -0500

    gintel/utils: add tokens skeleton

--- gintel/utils.py
+++ gintel/utils.py
@@ -1,7 +1,14 @@
 from pathlib import Path
+from pyeio import easy
 from gintel.env import FILES
+from gintel.env import TOKENS
 
 
 def make_package_directory():
     if not Path(FILES).exists():
         Path(FILES).mkdir()
+
+
+class Tokens:
+    def __init__(self) -> None:
+        pass

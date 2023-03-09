697f678f22fbda96db23c7979533d25b20c20f94
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-08 23:14:29 -0500

    setup: update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "requests"],
+    install_requires=["click", "requests", "pyeio"],
     entry_points={"console_scripts": ["gintel=gintel.cli:entry"]},
     cmdclass={
         "install": Installation,

001c91aa9870937b8c39c53d9765f25e087366df
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 18:23:56 -0500

    gintel/obj: add basic skeleton class for position

--- gintel/obj.py
+++ gintel/obj.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel
+import math
+from typing import Optional
+from pydantic.dataclasses import dataclass
 
 
-class Position(BaseModel):
-    pass
+@dataclass
+class Position:
+    zm: int
+    tx: Optional[int] = None
+    ty: Optional[int] = None
+    lt: Optional[float] = None
+    lg: Optional[float] = None
+
+    def __post_init__(self):
+        pass
+
+    def deg2num(lat_deg, lon_deg, zoom):
+        lat_rad = math.radians(lat_deg)
+        n = 2.0**zoom
+        xtile = int((lon_deg + 180.0) / 360.0 * n)
+        ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+        return (xtile, ytile)

107559548711b991c1c5f5d743ac95a881533438
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 17:36:22 -0500

    gintel/utils: add token interface class

--- gintel/utils.py
+++ gintel/utils.py
@@ -1,14 +1,73 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: base
+#     language: python
+#     name: python3
+# ---
+
+# %%
 from pathlib import Path
 from pyeio import easy
 from gintel.env import FILES
 from gintel.env import TOKENS
 
 
+# %%
 def make_package_directory():
     if not Path(FILES).exists():
         Path(FILES).mkdir()
 
 
+# %%
 class Tokens:
     def __init__(self) -> None:
-        pass
+        self.data = easy.load(TOKENS)
+
+    @property
+    def defined(self) -> list[str]:
+        return [k for k, v in self.data.items() if v is not None]
+
+    @property
+    def undefined(self) -> list[str]:
+        return [k for k, v in self.data.items() if v is None]
+
+    def save(self) -> None:
+        easy.save(self.data, TOKENS)
+
+    def get(self, name: str) -> str:
+        if name in self.data.keys():
+            return self.data[name]
+        else:
+            raise Exception("Invalid Token Name")
+
+    def update(self, name: str, token: str) -> None:
+        if name in self.data.keys():
+            self.data[name] = token
+            self.save()
+        else:
+            raise Exception("Invalid Token Name")
+
+    def remove(self, name: str, token: str) -> bool:
+        if name in self.data.keys():
+            self.data[name] = None
+            self.save()
+        else:
+            raise Exception("Invalid Token Name")
+
+
+# %%
+tokens = Tokens()
+
+# %%
+tokens.tokens
+
+# %%

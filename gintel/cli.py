619dc72fe4a42cf15dda53394d9518f2b5883da0
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 17:59:36 -0500

    gintel/cli: update yellow color string closer

--- gintel/cli.py
+++ gintel/cli.py
@@ -28,7 +28,7 @@ def view():
 @tokens.command()
 def add():
     if len(token_cache.undefined) == 0:
-        vprint("[yellow]No Undefined Tokens")
+        vprint("[yellow]No Undefined Tokens[/yellow]")
     else:
         selection = pick(options=token_cache.undefined, title="Select Token")
         vprint(f"{selection[0].title()} API Token")
@@ -40,7 +40,7 @@ def add():
 @tokens.command()
 def delete():
     if len(token_cache.defined) == 0:
-        vprint("[yellow]No Defined Tokens")
+        vprint("[yellow]No Defined Tokens[/yellow]")
     else:
         selection = pick(options=token_cache.defined, title="Select Token")
         token_cache.remove(selection[0])
@@ -50,7 +50,7 @@ def delete():
 @tokens.command()
 def change():
     if len(token_cache.defined) == 0:
-        vprint("[yellow]No Defined Tokens")
+        vprint("[yellow]No Defined Tokens[/yellow]")
     else:
         selection = pick(options=token_cache.defined, title="Select Token")
         vprint(f"New {selection[0].title()} API Token")
@@ -61,4 +61,11 @@ def change():
 
 @tokens.command()
 def copy():
-    pass
+    # if len(token_cache.defined) == 0:
+    #     vprint("[yellow]No Defined Tokens[/yellow]")
+    # else:
+    #     selection = pick(options=token_cache.defined, title="Select Token")
+    #     vprint(f"New {selection[0].title()} API Token")
+    #     api_token = str(input("❯ "))
+    #     token_cache.update(selection[0], api_token)
+    #     vprint(f"[green]{selection[0].title()} Token Saved[/green]")

b275a85d8155e4323330d30530f5e333313a8c4c
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 21:52:06 -0500

    gintel/obj: move zoom validation up

--- gintel/obj.py
+++ gintel/obj.py
@@ -56,7 +56,10 @@ class Position(BaseModel):
 
     @validator("zoom")
     def __validate_zoom(cls, zoom: int) -> int:
-        pass
+        if (zoom >= 0) and (zoom <= 19):
+            return zoom
+        else:
+            raise Exception("Zoom is not within allowed range: [0, 19]")
 
     @staticmethod
     def make(**kwargs) -> Position:
@@ -68,13 +71,6 @@ class Position(BaseModel):
 
 #%%
 class PositionBuilder:
-    @staticmethod
-    def validate_zoom(zoom: int) -> None:
-        if (zoom >= 0) and (zoom <= 19):
-            return zoom
-        else:
-            raise Exception("Zoom is not within allowed range: [0, 19]")
-
     @staticmethod
     def validate_latitude(latitude: float) -> float:
         if (latitude >= -90) and (latitude <= 90):

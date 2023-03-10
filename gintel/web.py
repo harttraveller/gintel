2bd1ae0ac3255617fcbf2c72579d78a47490942d
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 20:02:29 -0500

    gintel/web: add tokens import and instantiation

--- gintel/web.py
+++ gintel/web.py
@@ -1,8 +1,12 @@
 from typing import Any
 
+from gintel.utils import Tokens
+
+token_cache = Tokens()
+
 
 class Mapbox:
-    def __init__(self, token: str) -> None:
+    def __init__(self, token: str | None = None) -> None:
         self.token = token
 
     def query(self, **kwargs) -> Any:

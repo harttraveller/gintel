962e99aa3a7b6422e40f84fe470d4f3e6262e61a
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 19:49:37 -0500

    gintel/env: add ttcmap

--- gintel/env.py
+++ gintel/env.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
-# Local
+# Paths
 HOME = Path.home()
 FILES = HOME / ".gintel"
 CACHE = FILES / "cache"
 TOKENS = f"{FILES}/tokens.json"
+
+# Data
+TTC_MAP = {"NW": [0, 0], "NE": [1, 0], "SW": [0, 1], "SE": [1, 1], "CE": [0.5, 0.5]}

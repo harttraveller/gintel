dfa9f1c002c080108f512c81109d07b1a0624db7
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-09 20:01:25 -0500

    gintel/web: add query skel to mapbox class

--- gintel/web.py
+++ gintel/web.py
@@ -1,6 +1,12 @@
+from typing import Any
+
+
 class Mapbox:
     def __init__(self, token: str) -> None:
         self.token = token
+
+    def query(self, **kwargs) -> Any:
+        pass
 
 
 class Endpoint:
